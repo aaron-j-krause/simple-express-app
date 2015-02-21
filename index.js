69679389ad679704e43ef6c213f370231269b04b
Aaron Krause <aaronjkrause@gmail.com>
2015-02-20 19:52:43 -0800

    User create changed, password security added, authentication mid-way

--- index.js
+++ index.js
@@ -2,26 +2,42 @@ var express = require('express');
 var mongo = require('mongo');
 var mongoose = require('mongoose');
 var app = express();
+var passport = require('passport')
+
+var assignUserRoutes = require('./lib/routes/userRoutes');
+var assignPostRoutes = require('./lib/routes/postRoutes');
+var assignPassportStrat = require('./lib/passport_strat');
 
 //middleware
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
+//passport
+app.set('appSecret', process.env.SECRET || 'chaaaaaange');
+app.use(passport.initialize());
+assignPassportStrat(passport);
+
+
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 //routes
-var userRoute = require('./lib/routes/userRoutes');
-var postRoute = require('./lib/routes/postRoutes');
+var userRouter = express.Router();
+var postRouter = express.Router();
 
-app.use('/user', userRoute);
-app.use('/posts', postRoute);
+assignPostRoutes(postRouter);
+assignUserRoutes(userRouter, passport, app.get('appSecret'));
+
+app.use('/user', userRouter);
+app.use('/posts', postRouter);
 
 //db
 mongoose.connect('mongodb://localhost/db');
 
 app.get('/', function(req, res) {
-  res.send('fuuuck yeah');
+  res.send(JSON.stringify("fuuuuck yeah"));
 });
 
-var server = app.listen(3000);
+var server = app.listen((process.env.PORT || 3000), function(port){
+  console.log('listening on ' + (process.env.PORT || 3000))
+});
